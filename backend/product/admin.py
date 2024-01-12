7e1a014a5604d90ff6b5cb91d8e150b280d4cc3e
Aysun <104984443+aysunak1@users.noreply.github.com>
2024-01-12 21:47:19 +0300

    dosya ekleme

--- backend/product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Brand
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
--- backend/sitesettings/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Slider, SocialMedia
-
-# Register your models here.
-
-admin.site.register(Slider)
-admin.site.register(SocialMedia)
